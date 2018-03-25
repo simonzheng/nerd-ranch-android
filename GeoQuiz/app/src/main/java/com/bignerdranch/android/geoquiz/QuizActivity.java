aa9de7b2b7be59fd1952b940f3aa1db0b49a9ea7
Simon Zheng <simonjzheng@gmail.com>
2018-03-25 10:27:14 -0400

    Add Buttons to QuizActivity.java

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,8 +2,13 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    // note: m prefix on instance vars denote a member variable (Android convention)
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
